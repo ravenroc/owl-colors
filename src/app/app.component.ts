99322b966e83f5b7748eca97263d343ce2e15048
ravenroc <paris.jeremyj@gmail.com>
2018-11-02 22:57:03 -0400

    initial styling of the teams

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'OWL Colors';
+  title = 'The Colors of the Overwatch League';
 }
